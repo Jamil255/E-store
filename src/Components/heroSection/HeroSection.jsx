e66331901314ada58536d83b2d8d41108f27d0a1
unknown <jamilafzal255@gmail.com>
2024-04-07 01:14:40 +0500

    heroSection  ui done

---
+++ src/Components/heroSection/HeroSection.jsx
@@ -0,0 +1,9 @@
+const HeroSection = () => {
+    return (
+        <div>
+            <img className=" h-40 lg:h-full" src="https://react-and-firebase-ecommerce-for-beginners.vercel.app/img/hero1.png" alt="" />
+        </div>
+    );
+}
+
+export default HeroSection;
