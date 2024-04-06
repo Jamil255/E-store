c862c7bdbe3ce59edc6a23d5e071acd34ac0f2f6
unknown <jamilafzal255@gmail.com>
2024-04-07 01:18:10 +0500

    add testominal

---
+++ src/Components/testimonial/Testimonial.jsx
@@ -0,0 +1,87 @@
+/* eslint-disable react/no-unescaped-entities */
+
+const Testimonial = () => {
+  return (
+    <div>
+      <section className="text-gray-600 body-font mb-10">
+        {/* main  */}
+        <div className="container px-5 py-10 mx-auto">
+          {/* Heading  */}
+          <h1 className=" text-center text-3xl font-bold text-black">
+            Testimonial
+          </h1>
+          {/* para  */}
+          <h2 className=" text-center text-2xl font-semibold mb-10">
+            What our <span className=" text-pink-500">customers</span> are
+            saying
+          </h2>
+
+          <div className="flex flex-wrap -m-4">
+            {/* Testimonial 1 */}
+            <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
+              <div className="h-full text-center">
+                <img
+                  alt="testimonial"
+                  className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
+                  src="https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg"
+                />
+                <p className="leading-relaxed">
+                  Edison bulb retro cloud bread echo park, helvetica stumptown
+                  taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee
+                  ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut
+                  adaptogen squid fanny pack vaporware.
+                </p>
+                <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
+                <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">
+                  Jamil Afzal{' '}
+                </h2>
+              </div>
+            </div>
+
+            {/* Testimonial 2 */}
+            <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
+              <div className="h-full text-center">
+                <img
+                  alt="testimonial"
+                  className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
+                  src="https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg"
+                />
+                <p className="leading-relaxed">
+                  Edison bulb retro cloud bread echo park, helvetica stumptown
+                  taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee
+                  ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut
+                  adaptogen squid fanny pack vaporware.
+                </p>
+                <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
+                <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">
+                  Selah{' '}
+                </h2>
+              </div>
+            </div>
+
+            {/* Testimonial 3 */}
+            <div className="lg:w-1/3 lg:mb-0 p-4">
+              <div className="h-full text-center">
+                <img
+                  alt="testimonial"
+                  className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
+                  src="https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg"
+                />
+                <p className="leading-relaxed">
+                  Edison bulb retro cloud bread echo park, helvetica stumptown
+                  taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee
+                  ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut
+                  adaptogen squid fanny pack vaporware.
+                </p>
+                <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
+                <p className="text-gray-500">CTO</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Testimonial
