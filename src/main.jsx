083da413b796f6d136b1e850f638487e6e65960c
unknown <jamilafzal255@gmail.com>
2024-04-07 01:28:28 +0500

    broswer  routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from '@material-tailwind/react'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+  </BrowserRouter>
 )
