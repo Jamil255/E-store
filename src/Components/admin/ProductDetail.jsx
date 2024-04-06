75c88ab5eff972016a953f5f7567d87d02fae22e
unknown <jamilafzal255@gmail.com>
2024-04-07 04:35:21 +0500

    product  detail add

---
+++ src/Components/admin/ProductDetail.jsx
@@ -0,0 +1,64 @@
+const ProductDetail = () => {
+  return (
+    <div>
+      <div className="py-5 flex justify-between items-center">
+        {/* text  */}
+        <h1 className=" text-xl text-pink-300 font-bold">All Product</h1>
+        {/* Add Product Button  */}
+        <button className="px-5 py-2 bg-pink-50 border border-pink-100 rounded-lg">
+          Add Product
+        </button>
+      </div>
+
+      {/* table  */}
+      <div className="w-full overflow-x-auto">
+        <table className="w-full text-left border border-collapse sm:border-separate border-pink-100 text-pink-400">
+          <tbody>
+            <tr>
+              <th
+                scope="col"
+                className="h-12 px-6 text-md border-l first:border-l-0 border-pink-100 text-slate-700 bg-slate-100 font-bold fontPara"
+              >
+                S.No.
+              </th>
+              <th
+                scope="col"
+                className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-pink-100 text-slate-700 bg-slate-100"
+              >
+                Location Name
+              </th>
+              <th
+                scope="col"
+                className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-pink-100 text-slate-700 bg-slate-100"
+              >
+                Action
+              </th>
+              <th
+                scope="col"
+                className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-pink-100 text-slate-700 bg-slate-100"
+              >
+                Action
+              </th>
+            </tr>
+            <tr className="text-pink-300">
+              <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 ">
+                1.
+              </td>
+              <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
+                {'name'}
+              </td>
+              <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 text-green-500 cursor-pointer ">
+                Edit
+              </td>
+              <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-pink-100 stroke-slate-500 text-slate-500 text-red-500 cursor-pointer ">
+                Delete
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
+
+export default ProductDetail
