bc8d7dbfc1ce7851945fb31ed28571634768e52b
unknown <jamilafzal255@gmail.com>
2024-04-08 02:09:16 +0500

    auth compelete and signup

--- src/pages/registration/Signup.jsx
+++ src/pages/registration/Signup.jsx
@@ -1,11 +1,85 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Link } from 'react-router-dom'
+import { useContext, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import myContext from '../../context/myContext'
+import toast from "react-hot-toast";
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth, db } from '../../firebase/FirebaseConfig';
+import { Timestamp, addDoc, collection } from 'firebase/firestore';
+import Loader from '../../Components/loader/Loader';
 
 const Signup = () => {
+  const context = useContext(myContext)
+  const { loading, setLoading } = context
+
+  // navigate
+  const navigate = useNavigate()
+
+  // User Signup State
+  const [userSignup, setUserSignup] = useState({
+    name: '',
+    email: '',
+    password: '',
+    role: 'user',
+  })
+  const userSignupFunction = async () => {
+    // validation
+    if (
+      userSignup.name === '' ||
+      userSignup.email === '' ||
+      userSignup.password === ''
+    ) {
+      toast.error('All Fields are required')
+    }
+
+    setLoading(true)
+    try {
+      const users = await createUserWithEmailAndPassword(
+        auth,
+        userSignup.email,
+        userSignup.password
+      )
+
+      // create user object
+      const user = {
+        name: userSignup.name,
+        email: users.user.email,
+        uid: users.user.uid,
+        role: userSignup.role,
+        time: Timestamp.now(),
+        date: new Date().toLocaleString('en-US', {
+          month: 'short',
+          day: '2-digit',
+          year: 'numeric',
+        }),
+      }
+
+      // create user Refrence
+      const userRefrence = collection(db, 'user')
+
+      // Add User Detail
+      addDoc(userRefrence, user)
+
+      setUserSignup({
+        name: '',
+        email: '',
+        password: '',
+      })
+
+      toast.success('Signup Successfully')
+
+      setLoading(false)
+      navigate('/login')
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
   return (
     <div className="flex justify-center items-center h-screen">
+      {loading && <Loader />}
       {/* Login Form  */}
-      <div className="login_Form bg-pink-50 px-1 lg:px-8 py-6 border border-pink-100 rounded-xl shadow-md">
+      <div className="login_Form bg-pink-50 px-8 py-6 border border-pink-100 rounded-xl shadow-md">
         {/* Top Heading  */}
         <div className="mb-5">
           <h2 className="text-center text-2xl font-bold text-pink-500 ">
@@ -18,6 +92,13 @@ const Signup = () => {
           <input
             type="text"
             placeholder="Full Name"
+            value={userSignup.name}
+            onChange={(e) => {
+              setUserSignup({
+                ...userSignup,
+                name: e.target.value,
+              })
+            }}
             className="bg-pink-50 border border-pink-200 px-2 py-2 w-96 rounded-md outline-none placeholder-pink-200"
           />
         </div>
@@ -27,6 +108,13 @@ const Signup = () => {
           <input
             type="email"
             placeholder="Email Address"
+            value={userSignup.email}
+            onChange={(e) => {
+              setUserSignup({
+                ...userSignup,
+                email: e.target.value,
+              })
+            }}
             className="bg-pink-50 border border-pink-200 px-2 py-2 w-96 rounded-md outline-none placeholder-pink-200"
           />
         </div>
@@ -36,6 +124,13 @@ const Signup = () => {
           <input
             type="password"
             placeholder="Password"
+            value={userSignup.password}
+            onChange={(e) => {
+              setUserSignup({
+                ...userSignup,
+                password: e.target.value,
+              })
+            }}
             className="bg-pink-50 border border-pink-200 px-2 py-2 w-96 rounded-md outline-none placeholder-pink-200"
           />
         </div>
@@ -44,6 +139,7 @@ const Signup = () => {
         <div className="mb-5">
           <button
             type="button"
+            onClick={userSignupFunction}
             className="bg-pink-500 hover:bg-pink-600 w-full text-white text-center py-2 font-bold rounded-md "
           >
             Signup
