4996e4374100a2bfe83e1ad0b86d658ef3545c11
unknown <jamilafzal255@gmail.com>
2024-04-09 04:17:18 +0500

    category functionality add

--- src/Components/category/Category.jsx
+++ src/Components/category/Category.jsx
@@ -1,4 +1,4 @@
-// category
+import { useNavigate } from 'react-router-dom'
 const category = [
   {
     image: 'https://cdn-icons-png.flaticon.com/256/4359/4359963.png',
@@ -35,6 +35,7 @@ const category = [
 ]
 
 const Category = () => {
+  const navigate = useNavigate()
   return (
     <div>
       <div className="flex flex-col mt-5">
@@ -46,16 +47,15 @@ const Category = () => {
             {category.map((item, index) => {
               return (
                 <div key={index} className="px-3 lg:px-10">
-                  {/* Image  */}
-                  <div className=" w-16 h-16 lg:w-24 lg:h-24 max-w-xs rounded-full  bg-pink-500 transition-all hover:bg-pink-400 cursor-pointer mb-1 ">
+                  <div
+                    onClick={() => navigate(`/category/${item.name}`)}
+                    className=" w-16 h-16 lg:w-24 lg:h-24 max-w-xs rounded-full  bg-pink-500 transition-all hover:bg-pink-400 cursor-pointer mb-1 "
+                  >
                     <div className="flex justify-center mb-12">
-                      {/* Image tag  */}
                       <img src={item.image} alt="img" />
                     </div>
                   </div>
-
-                  {/* Name Text  */}
-                  <h1 className=" text-sm lg:text-lg text-center font-medium title-font first-letter:uppercase ">
+                  <h1 className=" text-sm lg:text-lg text-center font-medium title-font ">
                     {item.name}
                   </h1>
                 </div>
