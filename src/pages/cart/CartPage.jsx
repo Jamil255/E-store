48fc8f792c0e2f4001311d0d2cfe1b53cb9fce8e
unknown <jamilafzal255@gmail.com>
2024-04-10 01:02:12 +0500

    add to cart page  done

--- src/pages/cart/CartPage.jsx
+++ src/pages/cart/CartPage.jsx
@@ -1,46 +1,105 @@
+import { useDispatch, useSelector } from 'react-redux'
 import { Trash } from 'lucide-react'
+import {
+  decrementQuantity,
+  deleteFromCart,
+  incrementQuantity,
+} from '../../redux/cartSlice'
+import toast from 'react-hot-toast'
+import { useEffect, useState } from 'react'
+import { Timestamp, addDoc, collection } from 'firebase/firestore'
+import { db } from '../../firebase/FirebaseConfig'
+import { Navigate } from 'react-router'
 import Layout from '../../Components/Layout/Layout'
-
-const products = [
-  {
-    id: 1,
-    name: 'Nike Air Force 1 07 LV8',
-    href: '#',
-    price: '₹47,199',
-    originalPrice: '₹48,900',
-    discount: '5% Off',
-    color: 'Orange',
-    size: '8 UK',
-    imageSrc:
-      'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/54a510de-a406-41b2-8d62-7f8c587c9a7e/air-force-1-07-lv8-shoes-9KwrSk.png',
-  },
-  {
-    id: 2,
-    name: 'Nike Blazer Low 77 SE',
-    href: '#',
-    price: '₹1,549',
-    originalPrice: '₹2,499',
-    discount: '38% off',
-    color: 'White',
-    leadTime: '3-4 weeks',
-    size: '8 UK',
-    imageSrc:
-      'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e48d6035-bd8a-4747-9fa1-04ea596bb074/blazer-low-77-se-shoes-0w2HHV.png',
-  },
-  {
-    id: 3,
-    name: 'Nike Air Max 90',
-    href: '#',
-    price: '₹2219 ',
-    originalPrice: '₹999',
-    discount: '78% off',
-    color: 'Black',
-    imageSrc:
-      'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/fd17b420-b388-4c8a-aaaa-e0a98ddf175f/dunk-high-retro-shoe-DdRmMZ.png',
-  },
-]
+import BuyNowModal from '../../Components/buyNowModal/BuyNowModal'
 
 const CartPage = () => {
+  const cartItems = useSelector((state) => state.cart)
+  const dispatch = useDispatch()
+
+  const deleteCart = (item) => {
+    dispatch(deleteFromCart(item))
+    toast.success('Delete cart')
+  }
+
+  const handleIncrement = (id) => {
+    dispatch(incrementQuantity(id))
+  }
+
+  const handleDecrement = (id) => {
+    dispatch(decrementQuantity(id))
+  }
+
+  // const cartQuantity = cartItems.length;
+
+  const cartItemTotal = cartItems
+    .map((item) => item.quantity)
+    .reduce((prevValue, currValue) => prevValue + currValue, 0)
+
+  const cartTotal = cartItems
+    .map((item) => item.price * item.quantity)
+    .reduce((prevValue, currValue) => prevValue + currValue, 0)
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cartItems))
+  }, [cartItems])
+
+  // user
+  const user = JSON.parse(localStorage.getItem('users'))
+
+  // Buy Now Function
+  const [addressInfo, setAddressInfo] = useState({
+    name: '',
+    address: '',
+    pincode: '',
+    mobileNumber: '',
+    time: Timestamp.now(),
+    date: new Date().toLocaleString('en-US', {
+      month: 'short',
+      day: '2-digit',
+      year: 'numeric',
+    }),
+  })
+
+  const buyNowFunction = () => {
+    // validation
+    if (
+      addressInfo.name === '' ||
+      addressInfo.address === '' ||
+      addressInfo.pincode === '' ||
+      addressInfo.mobileNumber === ''
+    ) {
+      return toast.error('All Fields are required')
+    }
+
+    // Order Info
+    const orderInfo = {
+      cartItems,
+      addressInfo,
+      email: user.email,
+      userid: user.uid,
+      status: 'confirmed',
+      time: Timestamp.now(),
+      date: new Date().toLocaleString('en-US', {
+        month: 'short',
+        day: '2-digit',
+        year: 'numeric',
+      }),
+    }
+    try {
+      const orderRef = collection(db, 'order')
+      addDoc(orderRef, orderInfo)
+      setAddressInfo({
+        name: '',
+        address: '',
+        pincode: '',
+        mobileNumber: '',
+      })
+      toast.success('Order Placed Successfull')
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <Layout>
       <div className="container mx-auto px-4 max-w-7xl lg:px-0">
@@ -57,87 +116,94 @@ const CartPage = () => {
                 Items in your shopping cart
               </h2>
               <ul role="list" className="divide-y divide-gray-200">
-                {products.map((product) => (
-                  <div key={product.id} className="">
-                    <li className="flex py-6 sm:py-6 ">
-                      <div className="flex-shrink-0">
-                        <img
-                          src={product.imageSrc}
-                          alt={product.name}
-                          className="sm:h-38 sm:w-38 h-24 w-24 rounded-md object-contain object-center"
-                        />
-                      </div>
+                {cartItems.length > 0 ? (
+                  <>
+                    {cartItems.map((item, index) => {
+                      const {
+                        id,
+                        title,
+                        price,
+                        productImageUrl,
+                        quantity,
+                        category,
+                      } = item
+                      return (
+                        <div key={index} className="">
+                          <li className="flex py-6 sm:py-6 ">
+                            <div className="flex-shrink-0">
+                              <img
+                                src={productImageUrl}
+                                alt="img"
+                                className="sm:h-38 sm:w-38 h-24 w-24 rounded-md object-contain object-center"
+                              />
+                            </div>
 
-                      <div className="ml-4 flex flex-1 flex-col justify-between sm:ml-6">
-                        <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
-                          <div>
-                            <div className="flex justify-between">
-                              <h3 className="text-sm">
-                                <a
-                                  href={product.href}
-                                  className="font-semibold text-black"
-                                >
-                                  {product.name}
-                                </a>
-                              </h3>
+                            <div className="ml-4 flex flex-1 flex-col justify-between sm:ml-6">
+                              <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
+                                <div>
+                                  <div className="flex justify-between">
+                                    <h3 className="text-sm">
+                                      <div className="font-semibold text-black">
+                                        {title}
+                                      </div>
+                                    </h3>
+                                  </div>
+                                  <div className="mt-1 flex text-sm">
+                                    <p className="text-sm text-gray-500">
+                                      {category}
+                                    </p>
+                                  </div>
+                                  <div className="mt-1 flex items-end">
+                                    <p className="text-sm font-medium text-gray-900">
+                                      ₹{price}
+                                    </p>
+                                  </div>
+                                </div>
+                              </div>
                             </div>
-                            <div className="mt-1 flex text-sm">
-                              <p className="text-sm text-gray-500">
-                                {product.color}
-                              </p>
-                              {product.size ? (
-                                <p className="ml-4 border-l border-gray-200 pl-4 text-sm text-gray-500">
-                                  {product.size}
-                                </p>
-                              ) : null}
+                          </li>
+                          <div className="mb-2 flex">
+                            <div className="min-w-24 flex">
+                              <button
+                                onClick={() => handleDecrement(id)}
+                                type="button"
+                                className="h-7 w-7"
+                              >
+                                -
+                              </button>
+                              <input
+                                type="text"
+                                className="mx-1 h-7 w-9 rounded-md border text-center"
+                                value={quantity}
+                              />
+                              <button
+                                onClick={() => handleIncrement(id)}
+                                type="button"
+                                className="flex h-7 w-7 items-center justify-center"
+                              >
+                                +
+                              </button>
                             </div>
-                            <div className="mt-1 flex items-end">
-                              <p className="text-xs font-medium text-gray-500 line-through">
-                                {product.originalPrice}
-                              </p>
-                              <p className="text-sm font-medium text-gray-900">
-                                &nbsp;&nbsp;{product.price}
-                              </p>
-                              &nbsp;&nbsp;
-                              <p className="text-sm font-medium text-green-500">
-                                {product.discount}
-                              </p>
+                            <div className="ml-6 flex text-sm">
+                              <button
+                                onClick={() => deleteCart(item)}
+                                type="button"
+                                className="flex items-center space-x-1 px-2 py-1 pl-0"
+                              >
+                                <Trash size={12} className="text-red-500" />
+                                <span className="text-xs font-medium text-red-500">
+                                  Remove
+                                </span>
+                              </button>
                             </div>
                           </div>
                         </div>
-                      </div>
-                    </li>
-                    <div className="mb-2 flex">
-                      <div className="min-w-24 flex">
-                        <button type="button" className="h-7 w-7">
-                          -
-                        </button>
-                        <input
-                          type="text"
-                          className="mx-1 h-7 w-9 rounded-md border text-center"
-                          defaultValue={1}
-                        />
-                        <button
-                          type="button"
-                          className="flex h-7 w-7 items-center justify-center"
-                        >
-                          +
-                        </button>
-                      </div>
-                      <div className="ml-6 flex text-sm">
-                        <button
-                          type="button"
-                          className="flex items-center space-x-1 px-2 py-1 pl-0"
-                        >
-                          <Trash size={12} className="text-red-500" />
-                          <span className="text-xs font-medium text-red-500">
-                            Remove
-                          </span>
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                ))}
+                      )
+                    })}
+                  </>
+                ) : (
+                  <h1>Not Found</h1>
+                )}
               </ul>
             </section>
             {/* Order summary */}
@@ -154,17 +220,11 @@ const CartPage = () => {
               <div>
                 <dl className=" space-y-1 px-2 py-4">
                   <div className="flex items-center justify-between">
-                    <dt className="text-sm text-gray-800">Price (3 item)</dt>
-                    <dd className="text-sm font-medium text-gray-900">
-                      ₹ 52,398
-                    </dd>
-                  </div>
-                  <div className="flex items-center justify-between pt-4">
-                    <dt className="flex items-center text-sm text-gray-800">
-                      <span>Discount</span>
+                    <dt className="text-sm text-gray-800">
+                      Price ({cartItemTotal} item)
                     </dt>
-                    <dd className="text-sm font-medium text-green-700">
-                      - ₹ 3,431
+                    <dd className="text-sm font-medium text-gray-900">
+                      ₹ {cartTotal}
                     </dd>
                   </div>
                   <div className="flex items-center justify-between py-4">
@@ -178,15 +238,21 @@ const CartPage = () => {
                       Total Amount
                     </dt>
                     <dd className="text-base font-medium text-gray-900">
-                      ₹ 48,967
+                      ₹ {cartTotal}
                     </dd>
                   </div>
                 </dl>
                 <div className="px-2 pb-4 font-medium text-green-700">
                   <div className="flex gap-4 mb-6">
-                    <button className="w-full px-4 py-3 text-center text-gray-100 bg-pink-600 border border-transparent dark:border-gray-700 hover:border-pink-500 hover:text-pink-700 hover:bg-pink-100 rounded-xl">
-                      Buy now
-                    </button>
+                    {user ? (
+                      <BuyNowModal
+                        addressInfo={addressInfo}
+                        setAddressInfo={setAddressInfo}
+                        buyNowFunction={buyNowFunction}
+                      />
+                    ) : (
+                      <Navigate to={'/login'} />
+                    )}
                   </div>
                 </div>
               </div>
