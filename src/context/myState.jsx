19a8c751d655711ca449a0e98d095990f414b585
unknown <jamilafzal255@gmail.com>
2024-04-10 00:58:55 +0500

    all data get and add in conetxt using firebase done

--- src/context/myState.jsx
+++ src/context/myState.jsx
@@ -1,12 +1,23 @@
-import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
+/* eslint-disable react/prop-types */
 import { useEffect, useState } from 'react'
-import { db } from '../firebase/FirebaseConfig'
 import MyContext from './myContext'
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+} from 'firebase/firestore'
+import { db } from '../firebase/FirebaseConfig'
+import toast from 'react-hot-toast'
+
 function MyState({ children }) {
+  // Loading State
   const [loading, setLoading] = useState(false)
 
+  // User State
   const [getAllProduct, setGetAllProduct] = useState([])
-
   const getAllProductFunction = async () => {
     setLoading(true)
     try {
@@ -25,9 +36,62 @@ function MyState({ children }) {
       setLoading(false)
     }
   }
+  const [getAllOrder, setGetAllOrder] = useState([])
+  const getAllOrderFunction = async () => {
+    setLoading(true)
+    try {
+      const q = query(collection(db, 'order'), orderBy('time'))
+      const data = onSnapshot(q, (QuerySnapshot) => {
+        let orderArray = []
+        QuerySnapshot.forEach((doc) => {
+          orderArray.push({ ...doc.data(), id: doc.id })
+        })
+        setGetAllOrder(orderArray)
+        setLoading(false)
+      })
+      return () => data
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+  const deleteProduct = async (id) => {
+    setLoading(true)
+    try {
+      await deleteDoc(doc(db, 'order', id))
+      toast.success('Order Deleted successfully')
+      getAllOrderFunction()
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+  const [getAllUser, setGetAllUser] = useState([])
+
+  const getAllUserFunction = async () => {
+    setLoading(true)
+    try {
+      const q = query(collection(db, 'user'), orderBy('time'))
+      const data = onSnapshot(q, (QuerySnapshot) => {
+        let userArray = []
+        QuerySnapshot.forEach((doc) => {
+          userArray.push({ ...doc.data(), id: doc.id })
+        })
+        setGetAllUser(userArray)
+        setLoading(false)
+      })
+      return () => data
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
 
   useEffect(() => {
     getAllProductFunction()
+    getAllOrderFunction()
+    getAllUserFunction()
   }, [])
   return (
     <MyContext.Provider
@@ -36,6 +100,9 @@ function MyState({ children }) {
         setLoading,
         getAllProduct,
         getAllProductFunction,
+        getAllOrder,
+        deleteProduct,
+        getAllUser,
       }}
     >
       {children}
