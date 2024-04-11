cb41d793f1103700d0337ad0964d13d7c9dc23f5
unknown <jamilafzal255@gmail.com>
2024-04-12 02:05:41 +0500

    updated

--- src/pages/productInfo/ProductInfo.jsx
+++ src/pages/productInfo/ProductInfo.jsx
@@ -148,7 +148,7 @@ const ProductInfo = () => {
                         </ul>
                       </div>
                       <p className="inline-block text-2xl font-semibold text-gray-700 dark:text-gray-400 ">
-                        <span>₹ {product?.price}</span>
+                        <span>${product?.price}</span>
                       </p>
                     </div>
                     <div className="mb-6">
